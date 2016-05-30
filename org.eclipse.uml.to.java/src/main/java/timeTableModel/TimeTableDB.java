0a80feeb03c670b008448d0c56d95719c7bd4ab2
FHit <FHit@Florian>
2016-05-30 17:09:00 +0200

    TTDB #1

--- org.eclipse.uml.to.java/src/main/java/timeTableModel/TimeTableDB.java
+++ org.eclipse.uml.to.java/src/main/java/timeTableModel/TimeTableDB.java
@@ -2,6 +2,28 @@
  * 2016, All rights reserved.
  *******************************************************************************/
 package timeTableModel;
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Comment;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.IOException;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import java.util.HashSet;
 
@@ -28,8 +50,6 @@ public class TimeTableDB {
 	/**
 	 * Description of the property file.
 	 */
-	private Object file = ;
-
 	/**
 	 * Description of the property rooms.
 	 */
@@ -40,9 +60,19 @@ public class TimeTableDB {
 	// End of user code
 
 	/**
-	 * The constructor.
+	 * 
+	 * Le fichier contenant la base de donnÃ©es.
+	 * 
 	 */
-	public TimeTableDB() {
+	private String file;
+	/**
+	 * 
+	 * Constructeur de TimeTableDB. 
+	 * 
+	 * @param file
+	 * 		Le nom du fichier qui contient la base de donnÃ©es.
+	 */
+	public TimeTableDB(String file) {
 		// Start of user code constructor for TimeTableDB)
 		super();
 		// End of user code
@@ -51,9 +81,18 @@ public class TimeTableDB {
 	/**
 	 * Description of the method saveDB.
 	 */
-	public void saveDB() {
-		// Start of user code for method saveDB
-		// End of user code
+	public String document;
+	
+	public void saveDB(String file) {
+		try
+		   {
+		      //On utilise ici un affichage classique avec getPrettyFormat()
+		      XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
+		      //Remarquez qu'il suffit simplement de créer une instance de FileOutputStream
+		      //avec en argument le nom du fichier pour effectuer la sérialisation.
+		      sortie.output(document, new FileOutputStream(file));
+		   }
+		   catch (java.io.IOException e){}
 	}
 
 	/**
@@ -95,16 +134,16 @@ public class TimeTableDB {
 	 * Returns file.
 	 * @return file 
 	 */
-	public Object getFile() {
-		return this.file;
+	public String getFile() {
+		return file;
 	}
 
 	/**
 	 * Sets a value to attribute file. 
 	 * @param newFile 
 	 */
-	public void setFile(Object newFile) {
-		this.file = newFile;
+	public void setFile(String file) {
+		this.file = file;
 	}
 
 	/**
