8f85c2fc9186b48a4ec580ba603e23d0c3cb4b70
Eloi <Eloi@PCEloi>
2016-06-03 14:40:11 +0200

    #1 maj roomsidtostring et roomstostring

--- org.eclipse.uml.to.java/src/main/java/timeTableController/TimeTableController.java
+++ org.eclipse.uml.to.java/src/main/java/timeTableController/TimeTableController.java
@@ -2,6 +2,9 @@
  * 2016, All rights reserved.
  *******************************************************************************/
 package timeTableController;
+import java.util.*;
+import java.util.Map.Entry;
+
 
 // Start of user code (user defined imports)
 
@@ -53,5 +56,48 @@ public class TimeTableController implements ITimeTableController {
 		}
 		this.timeTableDB.set(this);
 	}
+	
+	/**
+	 * Get the values of the different roomId
+	 * @return TabRoomsId
+	 */
+	
+	public String[] roomsIdToString() {
+		// Start of user code for method roomsIdToString
+		HashMap <Integer,Room> RoomMap = this.RoomMap;
+		Set<Integer> listKeys=RoomMap.keySet();
+		int size =listKeys.size();
+		String TabRoomsId[] = new String[size];
+		int i =0;
+	    for (Entry<Integer, Room> entry : RoomMap.entrySet()){
+			int key=entry.getKey();
+	    	String cle = "" + key;
+	    	TabRoomsId[i]=cle;
+	    	i++;
+	    }
+	    return TabRoomsId;
+		// End of user code
+	}
 
+
+	/**
+	 * Get the values of the different timeTableId
+	 * @return TabTimeTableId
+	 */
+	public String[] roomsToString() {
+		// Start of user code for method roomsToString
+		HashMap <Integer,Room> TimeTableMap = this.TimeTableMap;    //récupération du hashmap global
+		Set<Integer> listKeys=TimeTableMap.keySet();				//récupération des clés du hashmap que l'ont met dans un tableau d'entiers
+		int size =listKeys.size();									//récupération de la longueur de la liste de clés
+		String TabTimeTableId[] = new String[size];					//initialisation du tableau de string qui sera la sortie
+		int i =0;													//initialisation de l'itérateur
+	    for (Entry<Integer, Room> entry : TimeTableMap.entrySet()){	//code du prof
+			int key=entry.getKey();									//récupération des int conversion en string remplissage du tableau de sortie
+	    	String cle = "" + key;
+	    	TabTimeTableId[i]=cle;
+	    	i++;
+	    }
+	    return TabTimeTableId;
+		// End of user code
+	}
 }
